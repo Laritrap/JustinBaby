acc9c25cbe7b369de74d91280a05d45ab3616e8e
Laritrap <larissa.trapp@escola.pr.gov.br>
2024-09-25 08:07:27 -0300

    aa

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function soma(numero1,numero2){
 }
 console.log(soma(3,2));
 
-function area(numero3){
+function areaquad(numero3){
     return numero3*numero3;
 }
-console.log(area(7));
+console.log(areaquad(7));
