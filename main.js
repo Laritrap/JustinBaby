9c7b49f31e5cfbd61a8f2fce1015ac4406c6c332
Laritrap <larissa.trapp@escola.pr.gov.br>
2024-09-25 08:15:11 -0300

    aaa

--- main.js
+++ main.js
@@ -7,3 +7,8 @@ function areaQuadrado(valorLado){
     return valorLado**2;
 }
 console.log(areaQuadrado(7));
+
+let calculoMedia = function(numero3,numero4,numero5){
+    return (numero3 + numero4 + numero5)/3;
+}
+console.log(calculoMedia(6,7,8));
