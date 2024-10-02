8af4e154cd0d430a11cd48263222aa7efc05ca6c
Laritrap <larissa.trapp@escola.pr.gov.br>
2024-10-02 08:16:12 -0300

    fff

--- main.js
+++ main.js
@@ -19,16 +19,16 @@
 
 // document.write(calculoMedia(num3,num4,num5));
 
-let estados=['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG',
-    'PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE',
-    'TO','DF'];
-let i=0;
-    while(i<estados.length){
-        console.log(i);
-        console.log(estados[i]);
-        i=i+1;
-}
-for (let i = 0; i < estados.length; i++) {
-    console.log(i);
-    console.log(estados[i]);    
-}
+// let estados=['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG',
+//     'PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE',
+//     'TO','DF'];
+// let i=0;
+//     while(i<estados.length){
+//         console.log(i);
+//         console.log(estados[i]);
+//         i=i+1;
+// }
+// for (let i = 0; i < estados.length; i++) {
+//     console.log(i);
+//     console.log(estados[i]);    
+// }
