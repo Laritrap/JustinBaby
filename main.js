9d30e10d626c68fff72b44e839cfe5a363ee5c21
Laritrap <larissa.trapp@escola.pr.gov.br>
2024-10-02 07:26:40 -0300

    A

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-function soma(numero1,numero2){
-    return numero1 + numero2;
-}
-console.log(soma(3,2));
+// function soma(numero1,numero2){
+//     return numero1 + numero2;
+// }
+// console.log(soma(3,2));
 
-function areaQuadrado(valorLado){
-    return valorLado**2;
-}
-console.log(areaQuadrado(7));
+// function areaQuadrado(valorLado){
+//     return valorLado**2;
+// }
+// console.log(areaQuadrado(7));
 
-let num3=parseInt(prompt('Digite o primeiro número para calular a média'));
-let num4=parseInt(prompt('Digite o segundo número para calular a média'));
-let num5=parseInt(prompt('Digite o terceiro número para calular a média'));
+// let num3=parseInt(prompt('Digite o primeiro número para calular a média'));
+// let num4=parseInt(prompt('Digite o segundo número para calular a média'));
+// let num5=parseInt(prompt('Digite o terceiro número para calular a média'));
 
 
-let calculoMedia = function(numero3,numero4,numero5){
-    return (numero3 + numero4 + numero5)/3;
-}
+// let calculoMedia = function(numero3,numero4,numero5){
+//     return (numero3 + numero4 + numero5)/3;
+// }
 
-document.write(calculoMedia(num3,num4,num5));
+// document.write(calculoMedia(num3,num4,num5));
 
