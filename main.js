ea5d7c580ffb4945ce73d0c8ef560f7a321be78f
Laritrap <larissa.trapp@escola.pr.gov.br>
2024-10-02 07:38:39 -0300

    aa

--- main.js
+++ main.js
@@ -20,4 +20,5 @@
 // document.write(calculoMedia(num3,num4,num5));
 
 let estados=['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG',
-    'PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF']
+    'PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE',
+    'TO','DF']
